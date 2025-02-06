932868030c19b10a0447b709037930a2c2c68f1f
Isaac Caires <zrfisaac@gmail.com>
2025-02-06 16:31:25 -0300

    zrfisaac.docker.local : 1.0.3

---
+++ termux/load.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+mkdir -p ../public
+$sudo docker compose -f ../docker-compose.yml -f docker-compose.yml up -d
---
+++ termux/purge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# [ zrfisaac ]
+
+# [ about ]
+# - author : Isaac Caires
+# . - email : zrfisaac@gmail.com
+# . - site : zrfisaac.github.io
+
+# [ bash ]
+command -v sudo >/dev/null 2>&1 && sudo="sudo" || sudo=""
+$sudo docker container stop local_termux
+$sudo docker container rm local_termux
+$sudo docker container stop local_alpine
+$sudo docker container rm local_alpine
